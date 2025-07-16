c99a8a9b7598dc5799cb5dd0e82ad37d552d53ad
Christopher Kenrick <christopher.kenrick@gmail.com>
2025-07-16 16:44:24 -0400

    Updated optimized images to render correctly on website

--- src/images/optimized/index.js
+++ src/images/optimized/index.js
@@ -1,29 +1,42 @@
 // Auto-generated image optimization index
 
+// Import all profile images as modules so Vite includes them in the build
+import profileImgLargeJpg from './profile_img-large.jpg';
+import profileImgLargeWebp from './profile_img-large.webp';
+import profileImgMediumJpg from './profile_img-medium.jpg';
+import profileImgMediumWebp from './profile_img-medium.webp';
+import profileImgPlaceholderJpg from './profile_img-placeholder.jpg';
+import profileImgSmallJpg from './profile_img-small.jpg';
+import profileImgSmallWebp from './profile_img-small.webp';
+import profileImgThumbnailJpg from './profile_img-thumbnail.jpg';
+import profileImgThumbnailWebp from './profile_img-thumbnail.webp';
+import profileImgXlargeJpg from './profile_img-xlarge.jpg';
+import profileImgXlargeWebp from './profile_img-xlarge.webp';
+
 export const profileImg = {
   large: {
-    jpg: '/src/images/optimized/profile_img-large.jpg',
-    webp: '/src/images/optimized/profile_img-large.webp',
+    jpg: profileImgLargeJpg,
+    webp: profileImgLargeWebp,
   },
   medium: {
-    jpg: '/src/images/optimized/profile_img-medium.jpg',
-    webp: '/src/images/optimized/profile_img-medium.webp',
+    jpg: profileImgMediumJpg,
+    webp: profileImgMediumWebp,
   },
   placeholder: {
-    jpg: '/src/images/optimized/profile_img-placeholder.jpg',
+    jpg: profileImgPlaceholderJpg,
     placeholder: () => import('./profile_img-placeholder.js').then(m => m.default),
   },
   small: {
-    jpg: '/src/images/optimized/profile_img-small.jpg',
-    webp: '/src/images/optimized/profile_img-small.webp',
+    jpg: profileImgSmallJpg,
+    webp: profileImgSmallWebp,
   },
   thumbnail: {
-    jpg: '/src/images/optimized/profile_img-thumbnail.jpg',
-    webp: '/src/images/optimized/profile_img-thumbnail.webp',
+    jpg: profileImgThumbnailJpg,
+    webp: profileImgThumbnailWebp,
   },
   xlarge: {
-    jpg: '/src/images/optimized/profile_img-xlarge.jpg',
-    webp: '/src/images/optimized/profile_img-xlarge.webp',
+    jpg: profileImgXlargeJpg,
+    webp: profileImgXlargeWebp,
   },
 };
 
